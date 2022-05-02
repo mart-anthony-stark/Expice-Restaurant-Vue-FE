94b550a60833fe8220562f739c5cd7f68f2d0a69
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-02 23:02:59 +0800

    Reset: app layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Nav-Bar />
+  <router-view />
 </template>
 
+<script setup>
+/* eslint-disable */
+import NavBar from "@/components/NavBar.vue";
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
 }
 </style>
