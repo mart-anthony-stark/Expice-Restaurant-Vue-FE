aeb10f7173fcd74dfddb58354151da23cded3cdc
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-03 22:48:10 +0800

    Add: aos

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@
 <script setup>
 /* eslint-disable */
 import NavBar from "@/components/NavBar.vue";
+import { onMounted } from "@vue/runtime-core";
+
+import AOS from "aos";
+
+onMounted(() => {
+  AOS.init();
+});
 </script>
 
 <style lang="scss">
@@ -15,6 +22,20 @@ import NavBar from "@/components/NavBar.vue";
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
 }
-#app {
+
+::-webkit-scrollbar {
+  width: 15px;
+  background: $primary;
+}
+::-webkit-scrollbar-thumb {
+  background: rgba(255, 255, 255, 0.6);
+  border-radius: 15px;
+}
+button {
+  cursor: pointer;
+}
+
+section {
+  min-height: 100vh;
 }
 </style>
