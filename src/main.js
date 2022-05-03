dd37f19b25b7270f6d6df61e9fccf298d7bb526a
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-03 22:48:19 +0800

    Add: store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+// importing AOS css style globally
+import "aos/dist/aos.css";
+
+createApp(App).use(store).use(router).mount("#app");
