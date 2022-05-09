049538b981d9e033c2df93827fe529507e959a57
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-09 14:08:43 +0800

    Add: download app section component

---
+++ src/components/DownloadApp.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+      <div class="left"></div>
+      <div class="right"></div>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+section {
+  background: $complementary;
+}
+</style>
