179347e57dec8acab4b9952e9be326c8f1e89a7d
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-09 14:30:47 +0800

    Add: download app section UI

--- src/components/DownloadApp.vue
+++ src/components/DownloadApp.vue
@@ -1,7 +1,30 @@
 <template>
   <section>
-      <div class="left"></div>
-      <div class="right"></div>
+    <div class="left">
+      <div class="heading">
+        <h1>Download App For</h1>
+        <h1>Exciting Deals</h1>
+      </div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique
+        provident.
+      </p>
+      <div class="buttons">
+        <button>
+          <img
+            src="../assets/download-app/GooglePlayBtn.png"
+            alt="Google Play"
+          />
+        </button>
+        <button>
+          <img src="../assets/download-app/AppleStore.png" alt="Apple Store" />
+        </button>
+        <button></button>
+      </div>
+    </div>
+    <div class="right">
+      <img src="../assets/download-app/Phone.png" alt="" />
+    </div>
   </section>
 </template>
 
@@ -16,5 +39,46 @@ export default defineComponent({
 <style lang="scss" scoped>
 section {
   background: $complementary;
+  @include center();
+  gap: 5rem;
+
+  .left {
+    @include gapped(1.5rem);
+    flex-direction: column;
+    .heading {
+      position: relative;
+
+      h1 {
+        color: $secondary;
+        font-size: 2rem;
+        font-weight: 400;
+      }
+
+      &::before {
+        content: "";
+        position: absolute;
+        height: 100%;
+        width: 1rem;
+        background: $primary;
+        left: -1.3rem;
+      }
+    }
+
+    p {
+      max-width: 400px;
+    }
+
+    .buttons {
+      @include gapped(1rem);
+      button {
+        background: none;
+        border: none;
+      }
+    }
+  }
+
+  .right img {
+    height: 100vh;
+  }
 }
 </style>
